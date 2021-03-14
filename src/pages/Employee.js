01c1b04f5fd60d35959444040b83f9fdf3f4735f
SergioAlmazan <sergioalmazan18@yahoo.com>
2021-03-13 19:48:19 -0600

    added js files

---
+++ src/pages/Employee.js
@@ -0,0 +1,39 @@
+import React from "react";
+import axios from "axios";
+import EmployeeData from "./EmployeeData";
+
+
+class Employee extends React.Component {
+  state = {
+    employeeRecords: [],
+  };
+  componentDidMount = () => {
+    axios
+      .get("https://randomuser.me/api/?results=500&nat=us")
+      .then((records) => {
+        let eData = records.data.results;
+        let recordStamp = [];
+        for (let i = 0; i < eData.length; i++) {
+          let erecord = {
+            name: eData[i].name.first + " " + eData[i].name.last,
+            email: eData[i].email,
+            phone: eData[i].cell,
+            dob: eData[i].dob.date,
+          };
+          recordStamp.push(erecord);
+        }
+        this.setState({ employeeRecords: recordStamp });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Employee Roster</h2>
+        <EmployeeData employees={this.state.employeeRecords} />
+      </div>
+    );
+  }
+}
+
+export default Employee;
---
+++ src/pages/EmployeeData.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { MDBDataTable } from "mdbreact";
+
+const EmployeeData = (props) => {
+  const data = {
+    columns: [
+      {
+        label: "Name",
+        field: "name",
+        sort: "asc",
+        width: 500,
+      },
+      {
+        label: "Email Address",
+        field: "email",
+        sort: "asc",
+        width: 500,
+      },
+      {
+        label: "Phone #",
+        field: "phone",
+        sort: "asc",
+        width: 300,
+      },
+      {
+        label: "DOB",
+        field: "dob",
+        sort: "asc",
+        width: 250,
+      }
+    ],
+    rows:props.employees
+  };
+  return <MDBDataTable sorting={true} striped bordered data={data} />;
+};
+
+export default EmployeeData;
